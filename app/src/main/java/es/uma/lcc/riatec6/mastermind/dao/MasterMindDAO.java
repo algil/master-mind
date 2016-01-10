ab6edaeda5c8248318850fb550604c9312e0b575
tomi <gandalf0000@gmail.com>
2016-01-10 21:00:35 +0100

    Add descending order by points

--- app/src/main/java/es/uma/lcc/riatec6/mastermind/dao/MasterMindDAO.java
+++ app/src/main/java/es/uma/lcc/riatec6/mastermind/dao/MasterMindDAO.java
@@ -71,7 +71,7 @@ public class MasterMindDAO implements IMasterMindDAO {
                                               null,
                                               null,
                                               null,
-                                              null,
+                                              ConstantsDb.DB_MASTERMIND_TABLE_FIELD_POINTS + " DESC",
                                               null);
 
             if (resultSet.moveToFirst()) {
